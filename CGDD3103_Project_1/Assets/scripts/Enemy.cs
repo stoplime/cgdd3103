6507a6845c120aad6d41c15997646ee96f123694
stoplime <slanimero@gmail.com>
2017-09-13 10:29:44 -0400

    enemy variables

--- CGDD3103_Project_1/Assets/scripts/Enemy.cs
+++ CGDD3103_Project_1/Assets/scripts/Enemy.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	public Vector3 initPos;
+
+	public float damage;
+
+	public float health;
+
+	public float maxHealth;
+
+	public float healingTime;
+
+	public float healingAmount;
+
+	public float speed;
+
+	public float fireSpeed;
+
+	private float timer;
+
 	// Use this for initialization
 	void Start () {
 		
