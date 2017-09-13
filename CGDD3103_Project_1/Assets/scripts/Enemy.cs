ee2bc93d67faf8702338a714c34d748fb79ef8cb
stoplime <slanimero@gmail.com>
2017-09-13 15:57:22 -0400

    health linked to enemy

--- CGDD3103_Project_1/Assets/scripts/Enemy.cs
+++ CGDD3103_Project_1/Assets/scripts/Enemy.cs
@@ -6,15 +6,37 @@ public class Enemy : MonoBehaviour {
 
 	public Vector3 initPos;
 
-	public float damage;
+	public float damage = 1;
 
-	public float health;
+	public float Health
+	{
+		get
+		{
+			return health;
+		}
+		set
+		{
+			if (value >= 0 && value <= maxHealth)
+			{
+				health = value;
+			}
+			else if(value > maxHealth)
+			{
+				health = maxHealth;
+			}
+			else
+			{
+				health = 0;
+			}
+		}
+	}
+	private float health;
 
-	public float maxHealth;
+	public float maxHealth = 100f;
 
-	public float healingTime;
+	public float healingTime = 20;
 
-	public float healingAmount;
+	public float healingAmount = 10f;
 
 	public float speed;
 
@@ -22,13 +44,31 @@ public class Enemy : MonoBehaviour {
 
 	private float timer;
 
+	public void TakeDamage(float dmg)
+	{
+		Health = health - dmg;
+		timer = healingTime;
+	}
+
 	// Use this for initialization
 	void Start () {
-		
+		health = maxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		timer -= Time.deltaTime;
+		if (timer <= 0)
+		{
+			timer = healingTime;
+			Health = health + healingAmount; 
+		}
+
+		if (health == 0)
+		{
+			Destroy(gameObject);
+		}
 	}
+
+
 }
--- CGDD3103_Project_1/Assets/scripts/Health.cs
+++ CGDD3103_Project_1/Assets/scripts/Health.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class Health : MonoBehaviour {
 
-	public float maximumHealth = 100f;
-	private float currentHealth;
+	GameObject parent;
+	Enemy script;
 	private float currentBarLength;
 	private Vector3 scaleOrg;
 
 	// Use this for initialization
 	void Start () {
 		scaleOrg = transform.localScale;
-		currentHealth = maximumHealth;
+		parent = transform.parent.gameObject;
+		script = parent.GetComponent<Enemy>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		currentBarLength = currentHealth / maximumHealth;
+		currentBarLength = script.Health/script.maxHealth;
 		transform.LookAt(Camera.main.transform);
 		transform.Rotate(0, 180, 0);
 
